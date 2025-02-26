c8476d61ac44e17f264cdc76dcd1f5542ba6cf2b
Arvind T <137916714+ArvindT22@users.noreply.github.com>
2025-02-26 09:11:52 +0530

    Delete quiz directory

--- quiz/quiz/src/main/java/com/quiz/quiz/Repository/LeaderboardRepo.java
+++
@@ -1,7 +0,0 @@
-package com.quiz.quiz.Repository;
-
-import com.quiz.quiz.Entitiy.Leaderboard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LeaderboardRepo extends JpaRepository<Leaderboard,Integer> {
-}
